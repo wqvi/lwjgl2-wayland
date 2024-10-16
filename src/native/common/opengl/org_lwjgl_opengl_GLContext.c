34dc618143ecf7c10d557aac3d2b64d6407a3166
wqvi <137255931+wqvi@users.noreply.github.com>
2024-10-16 09:24:39 -0700

    Changed extgl_getProcAddress to SDL_GL_GetProcAddress

--- src/native/common/opengl/org_lwjgl_opengl_GLContext.c
+++ src/native/common/opengl/org_lwjgl_opengl_GLContext.c
@@ -34,13 +34,12 @@
 #include "common_tools.h"
 #include "org_lwjgl_opengl_GLContext.h"
 #include "extgl.h"
+#define NO_SDL_GLEXT
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 JNIEXPORT jlong JNICALL Java_org_lwjgl_opengl_GLContext_ngetFunctionAddress(JNIEnv *env, jclass clazz, jlong function_name) {
-	char *str = (char *)(intptr_t)function_name;
-	void *ptr = extgl_GetProcAddress(str);
-	printf("%p = nGetFunctionAddress(%s)\n", ptr, str);
-	fflush(stdout);
-	return (jlong)(intptr_t)ptr;
+	return (jlong)(intptr_t)SDL_GL_GetProcAddress((char *)(intptr_t)function_name);
 }
 
 JNIEXPORT void JNICALL Java_org_lwjgl_opengl_GLContext_nLoadOpenGLLibrary(JNIEnv * env, jclass clazz) {
